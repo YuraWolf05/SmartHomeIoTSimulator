661de933b19e23dd2cfa068f4270b1691a23a696
FilinStudioUA <volchkov.yuraa@gmail.com>
2026-04-30 23:46:33 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using SmartHomeIoTSimulator.UI;
+
+namespace SmartHomeIoTSimulator;
+
+internal static class Program
+{
+    [STAThread]
+    static void Main()
+    {
+        ApplicationConfiguration.Initialize();
+        Application.Run(new MainForm());
+    }
+}
